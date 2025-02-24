11322c518cfbdf768e4bfcf16b72e8d20c6efcb0
tarunsharma <tarun.kr.sharma@hotmail.com>
2025-02-24 22:49:47 +0400

    feat: Added simple server with health check endpoint, gracefull shutdown

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-console.log('Hello, world!');
+require('dotenv').config()
+import express, {Request, Response} from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send("I am working");
+});
+  
+// Health endpoint that returns a simple status message
+app.get('/health', (req: Request, res: Response) => {
+    //TODO: Add additional health checkes like rpc reachablity, db reachability, etc.
+    res.json({ status: 'ok' });
+});
+  
+const server = app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+  
+
+process.on('SIGTERM', () => {
+    console.log('SIGTERM signal received: closing HTTP server')
+    server.close(() => {
+        console.log('HTTP server closed')
+    })
+})
